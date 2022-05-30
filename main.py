f4721b5f4b5c1aaf43ebca70a9757407402a3c0b
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-30 17:12:27 +0800

    rename server.py in main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+
+baloons = [{'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting weight': 300},
+           {'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting weight': 300},]
+
+@app.get('/api/baloons')
+def get_baloons():
+    return json.dumps(baloons)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
