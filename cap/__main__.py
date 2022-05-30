73d5a656c375ba58f8b522f624b95b1952fe82a0
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-30 20:05:07 +0800

    add __init.py__. Creat modul cap

---
+++ cap/__main__.py
@@ -0,0 +1,23 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+baloons = [{'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
+           {'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},]
+
+@app.get('/api/v1/baloons/')
+def get_baloons():
+    return json.dumps(baloons)
+
+
+@app.post('/api/v1/baloons/')
+def add_baloons():
+    payload = request.json
+    baloons.append(payload)
+    return payload
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
