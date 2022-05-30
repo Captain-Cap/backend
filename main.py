2e155d32a53d6552cee0abcd38dba5fcaf32fd0e
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-30 17:13:28 +0800

    fix: path v1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 baloons = [{'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting weight': 300},
            {'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting weight': 300},]
 
-@app.get('/api/baloons')
+@app.get('/api/v1/baloons/')
 def get_baloons():
     return json.dumps(baloons)
 
