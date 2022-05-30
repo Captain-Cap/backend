a0448a87f5ecef5b00a8f4f485a6db3f57ec93eb
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-30 19:31:21 +0800

    fix:  func add_baloons

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-baloons = [{'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting weight': 300},
-           {'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting weight': 300},]
+baloons = [{'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
+           {'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},]
 
 @app.get('/api/v1/baloons/')
 def get_baloons():
@@ -14,7 +14,9 @@ def get_baloons():
 
 @app.post('/api/v1/baloons/')
 def add_baloons():
-    payload = request.json()
+    payload = request.json
+    baloons.append(payload)
+    return payload
 
 
 if __name__ == "__main__":
