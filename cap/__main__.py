fbdc8788141b091f0f11be1a6d62e4fe73eadf94
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-31 09:11:12 +0800

    add func get_baloons_by_id

--- cap/__main__.py
+++ cap/__main__.py
@@ -1,11 +1,13 @@
 import json
 from flask import Flask, request
 
+
 app = Flask(__name__)
 
 
-baloons = [{'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
-           {'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},]
+baloons = [{'id': 1, 'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
+           {'id': 2, 'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},]
+
 
 @app.get('/api/v1/baloons/')
 def get_baloons():
@@ -17,6 +19,11 @@ def add_baloons():
     payload = request.json
     baloons.append(payload)
     return payload
+
+
+@app.get('/api/v1/baloons/<int:id>/')
+def get_baloon_by_id(id):
+    return "<h1>{}</h1>".format(id)
 
 
 if __name__ == "__main__":
