abc3348f1abf72a095284812a053d06135fd93d0
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-31 11:20:14 +0800

    fix func changed_balloon

--- cap/__main__.py
+++ cap/__main__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 baloons = [{'id': 1, 'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
-           {'id': 1, 'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},
+           {'id': 2, 'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},
         ]
 
 @app.get('/api/v1/baloons/')
@@ -18,6 +18,17 @@ def add_baloons():
     payload = request.json
     baloons.append(payload)
     return payload
+
+
+@app.put('/api/v1/baloons/')
+def changed_balloon():
+    payload = request.json
+
+    for baloon in baloons:
+        if baloon['id'] == payload['id']:
+            baloons[baloons.index(baloon)] = payload
+            return payload
+    raise IndexError('id does not exist')
 
 
 if __name__ == "__main__":
