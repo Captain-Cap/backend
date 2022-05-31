02bc343302cd2aab5689af4598929042f45babc7
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-05-31 19:33:40 +0800

    fix: changed list by dict

--- cap/__main__.py
+++ cap/__main__.py
@@ -5,9 +5,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-baloons = [{'id': 1, 'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
-           {'id': 2, 'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},
-        ]
+baloons = {
+    1: {'id': 1, 'Firm': 'Maker Street', 'paint_code': 'ms400-504', 'color': 'grey-blue', 'volume': 400, 'starting_weight': 300},
+    2: {'id': 2, 'Firm': 'Maker Street', 'paint_code': 'ms400-101', 'color': 'yellow', 'volume': 400, 'starting_weight': 300},
+}
+        
 
 @app.get('/api/v1/baloons/')
 def get_baloons():
