6c51db82a9f9f7dadafd8636bea4c4e8819ff8c0
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-01 19:05:11 +0800

    add func add and delete

--- cap/storage.py
+++ cap/storage.py
@@ -6,3 +6,16 @@ class BaloonsStorage:
 
     def __init__(self):
         self.storage = {}
+
+
+    def add(self, balloon):
+        uid = balloon['id']
+        if self.storage.get(uid):
+            raise ValueError("such id already exists")
+        self.storage[uid] = balloon
+
+
+    def delete(self, uid):
+        if not self.storage.get(uid):
+            raise KeyError("balloon not found")
+        del self.storage[uid]
