c88e383496d3b653b31592ae7ecfef44e1776f79
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-02 15:53:09 +0800

    add name self.balloons

--- cap/storage.py
+++ cap/storage.py
@@ -1,6 +1,7 @@
 from cap.exception import ConflictError, NotFoundError
 
 class BalloonsStorage:
+    name = 'balloons'
 
     def __init__(self):
         self.storage = {}
@@ -9,26 +10,26 @@ class BalloonsStorage:
     def add(self, balloon):
         uid = balloon['id']
         if self.storage.get(uid):
-            raise ConflictError("balloons", "such id already exists")
+            raise ConflictError(self.name, "such id already exists")
         self.storage[uid] = balloon
 
 
     def delete(self, uid):
         if not self.storage.get(uid):
-            raise NotFoundError("balloons", "balloon not found")
+            raise NotFoundError(self.name, "balloon not found")
         del self.storage[uid]
 
 
     def update(self, balloon):
         uid = balloon['id']
         if not self.storage.get(uid):
-            raise NotFoundError("balloons", "balloon not found")
+            raise NotFoundError(self.name, "balloon not found")
         self.storage[uid] = balloon
 
 
     def get_balloon_by_id(self, uid):
         if not self.storage.get(uid):
-            raise NotFoundError("balloons", "balloon not found")
+            raise NotFoundError(self.name, "balloon not found")
         return self.storage[uid]
 
 
