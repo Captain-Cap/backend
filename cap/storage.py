450386723892e6272be2b4ec7a2a29a8413198b1
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-02 20:34:57 +0800

    add logging

--- cap/storage.py
+++ cap/storage.py
@@ -1,4 +1,8 @@
+import logging
 from cap.errors import ConflictError, NotFoundError
+
+logging.basicConfig(level=logging.ERROR)
+loger = logging.getLogger('storage')
 
 class BalloonsStorage:
     name = 'balloons'
@@ -10,12 +14,14 @@ class BalloonsStorage:
     def add(self, balloon):
         uid = balloon['id']
         if self.storage.get(uid):
+            loger.error(f"trying to add an existing id {uid}")
             raise ConflictError(self.name, f"reason: id {uid} already exists")
         self.storage[uid] = balloon
 
 
     def delete(self, uid):
         if not self.storage.get(uid):
+            loger.error(f"trying to delete a non-existent id {uid}")
             raise NotFoundError(self.name, f"reason: balloon id {uid} not found")
         del self.storage[uid]
 
@@ -23,12 +29,14 @@ class BalloonsStorage:
     def update(self, balloon):
         uid = balloon['id']
         if not self.storage.get(uid):
+            loger.error(f"trying to change a non-existent id {uid}")
             raise NotFoundError(self.name, f"reason: balloon id {uid} not found")
         self.storage[uid] = balloon
 
 
     def get_balloon_by_id(self, uid):
         if not self.storage.get(uid):
+            loger.error(f"calling a non-existent id {uid}")
             raise NotFoundError(self.name, f"reason: balloon id {uid} not found")
         return self.storage[uid]
 
