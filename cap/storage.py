cba023a53cd5d34948a35b8d4a3ea67f53a6442d
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-02 20:47:14 +0800

    fix: rename loger on logger. Degab, __name__

--- cap/storage.py
+++ cap/storage.py
@@ -1,8 +1,8 @@
 import logging
 from cap.errors import ConflictError, NotFoundError
 
-logging.basicConfig(level=logging.ERROR)
-loger = logging.getLogger('storage')
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 class BalloonsStorage:
     name = 'balloons'
@@ -14,14 +14,14 @@ class BalloonsStorage:
     def add(self, balloon):
         uid = balloon['id']
         if self.storage.get(uid):
-            loger.error(f"trying to add an existing id {uid}")
+            logger.error(f"trying to add an existing id {uid}")
             raise ConflictError(self.name, f"reason: id {uid} already exists")
         self.storage[uid] = balloon
 
 
     def delete(self, uid):
         if not self.storage.get(uid):
-            loger.error(f"trying to delete a non-existent id {uid}")
+            logger.error(f"trying to delete a non-existent id {uid}")
             raise NotFoundError(self.name, f"reason: balloon id {uid} not found")
         del self.storage[uid]
 
@@ -29,14 +29,14 @@ class BalloonsStorage:
     def update(self, balloon):
         uid = balloon['id']
         if not self.storage.get(uid):
-            loger.error(f"trying to change a non-existent id {uid}")
+            logger.error(f"trying to change a non-existent id {uid}")
             raise NotFoundError(self.name, f"reason: balloon id {uid} not found")
         self.storage[uid] = balloon
 
 
     def get_balloon_by_id(self, uid):
         if not self.storage.get(uid):
-            loger.error(f"calling a non-existent id {uid}")
+            logger.error(f"calling a non-existent id {uid}")
             raise NotFoundError(self.name, f"reason: balloon id {uid} not found")
         return self.storage[uid]
 
