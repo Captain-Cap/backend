dbf2c3908b7a64fac23dcac369a8d796f60499ed
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-03 22:22:12 +0800

    add handle error pydantic

--- cap/__main__.py
+++ cap/__main__.py
@@ -1,5 +1,6 @@
 import logging
 
+from pydantic import ValidationError
 from flask import Flask
 from cap.views import balloons
 from cap.errors import AppError
@@ -12,10 +13,15 @@ def handle_app_errors(error: AppError):
     logger.warning(error.reason)
     return {'error': error.reason}, error.status
 
+def handle_pydantic_errors(error: ValidationError):
+    logger.warning(str(error))
+    return {'error': error.errors()}, 422
+
 
 app = Flask(__name__)
 app.register_blueprint(balloons.routes, url_prefix='/api/v1/balloons/')
 app.register_error_handler(AppError, handle_app_errors)
+app.register_error_handler(ValidationError, handle_pydantic_errors)
 
 
 if __name__ == "__main__":
