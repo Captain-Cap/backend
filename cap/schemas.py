45bf586305a131027dc6c71398c3345e365f5897
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-04 18:45:15 +0800

    rename uid id

--- cap/schemas.py
+++ cap/schemas.py
@@ -2,10 +2,9 @@ from typing import Optional
 from pydantic import BaseModel, conint
 
 
-
 class CorrectBalloon(BaseModel):
 
-    id: Optional[conint(gt=0)]
+    uid: Optional[conint(gt=0)]
     firm: str
     paint_code: str
     color: str
