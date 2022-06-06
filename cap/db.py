0f933b79b678c0922f3a6d21ba72afae3e535f25
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-06 20:19:45 +0800

    feat:  add db.py. Сreated a database connection

---
+++ cap/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+engine = create_engine("postgresql://juijktrn:ov28zgV-BuuLiYDn-1ZixwKBZwwXSw5f@hattie.db.elephantsql.com/juijktrn")
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
