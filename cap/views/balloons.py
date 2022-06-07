12b5ee25a449034a6110a576aacea0c485d6c9aa
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-07 17:12:50 +0800

    refactor: update CorrectBalloon.from_orm

--- cap/views/balloons.py
+++ cap/views/balloons.py
@@ -1,43 +1,29 @@
-import json
+import orjson
 import logging
 from flask import request, Blueprint
 from cap.schemas import CorrectBalloon
 
-from cap.storageSQL import BalloonsStorageSQL
-from cap.models import Balloons
+from cap.sql_storage import BalloonsStorageSQL
 
 
 logger = logging.getLogger(__name__)
 
 routes = Blueprint("balloons", __name__)
-storageSQL = BalloonsStorageSQL()
-
-
-def get_json(balloon: Balloons):
-    json = {
-        "uid": balloon.uid,
-        "firm": balloon.firm,
-        "paint_code": balloon.paint_code,
-        "color": balloon.color,
-        "volume": balloon.volume,
-        "weight": balloon.weight,
-        "created": balloon.created,
-    }
-    return json
+sql_storage = BalloonsStorageSQL()
 
 
 @routes.get('/')
 def get_balloons():
     logger.debug("request get balloons")
-    balloons =  storageSQL.get_all()
-    return json.dumps([get_json(balloon) for balloon in balloons], default=str)
+    balloons =  sql_storage.get_all()
+    return orjson.dumps([CorrectBalloon.from_orm(entity).dict() for entity in balloons])
 
 
 @routes.get('/<int:uid>')
 def get_balloon_by_id(uid):
     logger.debug("request get balloon on id")
-    enitity = storageSQL.get_balloon_by_id(uid)
-    return get_json(enitity)
+    entity = sql_storage.get_balloon_by_id(uid)
+    return CorrectBalloon.from_orm(entity).dict()
 
 
 @routes.post('/')
@@ -45,20 +31,20 @@ def add_balloon():
     logger.debug("request add balloon")
     payload = request.json
     balloon = CorrectBalloon(**payload)
-    balloon = storageSQL.add(balloon)
-    return get_json(balloon)
+    entity = sql_storage.add(balloon)
+    return CorrectBalloon.from_orm(entity).dict()
 
 
 @routes.delete('/<int:uid>')
 def del_balloon(uid):
     logger.debug("request delete balloon")
-    storageSQL.delete(uid)
+    sql_storage.delete(uid)
     return {}, 204
 
 
-@routes.put('/<int:uid>')
-def change_balloon(uid):
+@routes.put('/')
+def change_balloon():
     payload = request.json
     balloon = CorrectBalloon(**payload)
-    balloon = storageSQL.update(balloon)
-    return get_json(balloon)
+    entity = sql_storage.update(balloon)
+    return CorrectBalloon.from_orm(entity).dict()
