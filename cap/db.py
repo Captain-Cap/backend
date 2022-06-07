1f76ed5721fffcfac041fcc8b18eb42ce0d0ad9d
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-06-07 15:08:12 +0800

    refactor: cap.secrets

--- cap/db.py
+++ cap/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-from secrets import postgresql
+from cap.secrets import postgresql
 
 
 engine = create_engine(postgresql)
